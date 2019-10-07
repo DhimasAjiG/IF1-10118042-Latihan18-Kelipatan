56689296e57088d6b8f002bbafd0ea1b2ac15457
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-10-07 18:56:37 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan18-Kelipatan/src/if1/pkg10118042/latihan18/kelipatan/IF110118042Latihan18Kelipatan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan18.kelipatan;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program   : Program ini berisi program Kelipatan
+ */
+public class IF110118042Latihan18Kelipatan {
+
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println(i*3.5);
+        }
+    }
+}
